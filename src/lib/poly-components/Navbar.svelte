55021276a0fc7827b97d958c002a87fb94372a13
ArrayBuffer01 <gamesawayhd@gmail.com>
2026-05-29 22:19:44 +0200

    INit

---
+++ src/lib/poly-components/Navbar.svelte
@@ -0,0 +1,86 @@
+<nav class="bg-plw-white fixed flex h-15 w-screen justify-center">
+	<div class="flex h-full w-300 items-center justify-between text-white">
+		<!-- Aligned left -->
+		<div class="flex items-center">
+			<a href="#" class="flex cursor-pointer items-center p-1">
+				<img src="logo.png" class="h-15 w-15 select-none" />
+				<h2 class="text-plw-black ml-5 text-xl font-extrabold select-none">Polyworlds</h2>
+			</a>
+			<span class="w-8"></span>
+
+			<!-- Main nav buttons -->
+			<div class="flex h-full">
+				<button
+					class="bg-plw-white text-plw-black hover:bg-neutral-150 flex h-15 cursor-pointer items-center px-4 font-semibold transition duration-200 select-none"
+				>
+					<i class="fa fa-gamepad"></i><span class="ml-2">Worlds</span>
+				</button>
+				<button
+					class="bg-plw-white text-plw-black hover:bg-neutral-150 flex h-15 cursor-pointer items-center px-4 font-semibold transition duration-200 select-none"
+				>
+					<i class="fa fa-home"></i><span class="ml-2">Groups</span>
+				</button>
+				<button
+					class="bg-plw-white text-plw-black hover:bg-neutral-150 flex h-15 cursor-pointer items-center px-4 font-semibold transition duration-200 select-none"
+				>
+					<i class="fa fa-shopping-basket"></i><span class="ml-2">Catalog</span>
+				</button>
+				<button
+					class="bg-plw-white text-plw-black hover:bg-neutral-150 flex h-15 cursor-pointer items-center px-4 font-semibold transition duration-200 select-none"
+				>
+					<i class="fa fa-group"></i><span class="ml-2">Forums</span>
+				</button>
+			</div>
+		</div>
+
+		<!-- Aligned right -->
+		<div class="flex items-center">
+			<p class="text-plw-black font-semibold">
+				<i class="fa fa-fire text-amber-500"></i>
+				<span class="ml-2 text-amber-500 select-none">0</span>
+			</p>
+			<span class="w-4"></span>
+			<p class="text-plw-black font-semibold">
+				<i class="fa fa-coins text-green-400"></i>
+				<span class="ml-2 text-green-400 select-none">0</span>
+			</p>
+			<span class="w-3"></span>
+
+			<!-- User button thingy -->
+			<div class="relative" id="dropdown-wrapper">
+				<button
+					id="dropdown-button"
+					class="text-plw-black hover:bg-neutral-150 flex h-15 cursor-pointer items-center p-3 font-semibold transition duration-200 select-none"
+				>
+					<span>Username</span>
+					<img src="profile.png" class="border-neutral-250 ml-2 h-10 w-10 rounded-full border" />
+				</button>
+
+				<!-- Dropdown menu -->
+				<div
+					id="dropdown-menu"
+					class="border-neutral-250 absolute top-full right-0 z-50 mt-2 hidden w-48 -translate-y-2 scale-100 transform rounded-lg border bg-white shadow-lg transition-transform duration-150"
+				>
+					<ul class="text-plw-black text-sm">
+						<li
+							class="text-plw-black mt-1 cursor-pointer px-4 py-2 font-semibold transition duration-100 select-none hover:bg-zinc-100"
+						>
+							Profile
+						</li>
+						<li
+							class="text-plw-black cursor-pointer px-4 py-2 font-semibold transition duration-100 select-none hover:bg-zinc-100"
+						>
+							Settings
+						</li>
+						<hr class="m-2 mr-5 ml-5 justify-center text-gray-200" />
+						<li
+							class="text-plw-red mb-1 cursor-pointer px-4 py-2 font-semibold transition duration-100 select-none hover:bg-rose-50"
+						>
+							Logout
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</nav>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,45 @@
+<div class="flex h-screen items-center justify-center">
+	<div
+		class="border-neutral-250 flex h-auto w-sm flex-col space-y-3 rounded-md border bg-white p-5 pl-6"
+	>
+		<div class="flex w-full">
+			<div class="h-20 w-2/3">
+				<p class="font-bold">Login to your account</p>
+				<p class="text-[15px] text-neutral-400">
+					Enter your account details below to login to your account
+				</p>
+			</div>
+			<div class="flex w-1/3 items-start justify-end">
+				<a class="p-2 pr-3 font-semibold hover:underline" href="#">Signup</a>
+			</div>
+		</div>
+
+		<form action="#">
+			<label for="email" class="text-left font-bold">Email</label>
+			<input
+				type="text"
+				id="email"
+				name="email"
+				placeholder="email@example.com"
+				class="border-neutral-250 mt-1 mb-6 w-full rounded-sm border-1 p-1 pl-3"
+			/>
+			<label for="pass" class="text-left font-bold">Password</label>
+			<input
+				type="password"
+				id="pass"
+				name="pass"
+				class="border-neutral-250 mt-1 mb-8 w-full rounded-sm border-1 p-1 pl-3"
+			/>
+			<input
+				type="submit"
+				value="Login"
+				class="bg-plw-red -mb-1 w-full rounded-md p-1.5 text-white"
+			/>
+		</form>
+
+		<button
+			class="text-plw-red hover:bg-plw-gray w-full rounded-md bg-transparent p-1.5 transition duration-200"
+			>Forgot your password?</button
+		>
+	</div>
+</div>
