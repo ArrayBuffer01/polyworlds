8c6179c23ca0252bde31731c8c0b29f00a030c4b
ArrayBuffer01 <gamesawayhd@gmail.com>
2026-07-22 16:33:22 +0200

    Add forums content

--- src/routes/forums/+page.svelte
+++ src/routes/forums/+page.svelte
@@ -3,3 +3,17 @@
 
   let { data }: PageProps = $props();
 </script>
+
+<div class="p-20">
+  {#if data.forums.length < 1}
+    <h1>No Forums Found.</h1>
+  {:else}
+    <div class="flex flex-col gap-2">
+      {#each data.forums as forum (forum.id)}
+        <div class="border border-plw-red">
+          <a href="/forums/{forum.id}">{forum.name}</a>
+        </div>
+      {/each}
+    </div>
+  {/if}
+</div>
