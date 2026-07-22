a6a92c5cfc97da76d516713d57a91564b2e1f285
ArrayBuffer01 <gamesawayhd@gmail.com>
2026-07-22 16:33:34 +0200

    Forums load func

--- src/routes/forums/+page.server.ts
+++ src/routes/forums/+page.server.ts
@@ -1,5 +1,9 @@
+import { db } from "$lib/server/db";
+import { forumsTable } from "$lib/server/db/schema";
 import type { PageServerLoad } from "./$types";
 
 export const load = (async () => {
-  return {};
+  const forums = await db.select().from(forumsTable);
+  console.log(forums);
+  return { forums };
 }) satisfies PageServerLoad;
