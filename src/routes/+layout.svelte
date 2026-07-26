257bf07b9ec264a9397a3385166ff2a8ad32d030
ArrayBuffer01 <gamesawayhd@gmail.com>
2026-07-26 14:33:22 +0200

    Remove unnecessary imports from root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
-  import { page } from "$app/state";
   import "./layout.css";
+
   import favicon from "$lib/assets/favicon.png";
-  import Navbar from "$lib/poly-components/Navbar.svelte";
   import { AppState, setAppContext } from "$lib/appState.svelte";
-  import LandingNav from "$lib/poly-components/LandingNav.svelte";
   import { untrack } from "svelte";
   import ThemeComp from "$lib/poly-components/ThemeComp.svelte";
 
